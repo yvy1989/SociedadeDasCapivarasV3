a999b82c1ecaa1a0e99461f599663d22a2bef067
João Marcos <joaomarcostroquez@gmail.com>
2022-09-14 20:52:11 -0400

    ADDED old NavMesh implementation

--- Assets/Scripts/AI/Enemy.cs
+++ Assets/Scripts/AI/Enemy.cs
@@ -11,36 +11,20 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-    }
 
-    // Start is called before the first frame update
-    public void EnableNavMeshAgent()
-    {    
         _navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
-        _navMeshAgent.enabled = true;
         _navMeshAgent.isStopped = false;
     }
 
     private void LateUpdate()
     {
-        if(_navMeshAgent != null)
-        {
-            float _distance = Vector3.Distance(transform.position, player.transform.position);
-            if (_distance > 2f && _distance < 10f)
-                _navMeshAgent.isStopped = false;
-            else
-                _navMeshAgent.isStopped = true;
+        float _distance = Vector3.Distance(transform.position, player.transform.position);
+        if (_distance > 2f && _distance < 10f)
+            _navMeshAgent.isStopped = false;
+        else
+            _navMeshAgent.isStopped = true;
 
-            if (!_navMeshAgent.isStopped)
-                _navMeshAgent.destination = player.transform.position;                
-        }
-    }
-
-    private IEnumerator AttackWithTimer(bool switchIsOn, float cooldown)
-    {
-        
-        switchIsOn = false;
-        yield return new WaitForSeconds(cooldown);
-        switchIsOn = true;
+        if (!_navMeshAgent.isStopped)
+            _navMeshAgent.destination = player.transform.position;                
     }
 }
