1835cbc6c65099868d08ed6fefd4f21d2519d1a7
João Marcos <joaomarcostroquez@gmail.com>
2022-09-20 22:38:51 -0400

    inicio da implementacao dos States da state machine da IA

---
+++ Assets/Scripts/AI/State.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class State
+{
+    public enum STATE
+    {
+        IDLE, PATROL, PURSUE, ATTACK
+    };
+
+    public enum EVENT
+    {
+        ENTER, UPDATE, EXIT
+    };
+
+    public STATE name;
+    protected EVENT stage;
+    protected GameObject npc;
+    protected Animator anim;
+    protected Transform player;
+    protected State nextState;
+    protected NavMeshAgent agent;
+
+    float visDist = 10f;
+    float visAngle = 30f;
+    float atkDist = 3f;
+
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+    {
+        npc = _npc;
+        agent = _agent;
+        anim = _anim;
+        stage = EVENT.ENTER;
+        player = _player;
+    }
+
+    public virtual void Enter() { stage = EVENT.UPDATE; }
+    public virtual void Update() { stage = EVENT.UPDATE; }
+    public virtual void Exit() { stage = EVENT.EXIT; }
+
+    public State Process()
+    {
+        if (stage == EVENT.ENTER) Enter();
+        if (stage == EVENT.UPDATE) Update();
+        if (stage == EVENT.EXIT)
+        {
+            Exit();
+            return nextState;
+        }
+        return this;
+    }
+}
