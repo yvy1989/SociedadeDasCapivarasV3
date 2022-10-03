172d7c4b4946ee0b46dfb3570610b730979d2869
yvy1989 <99916213+yvy1989@users.noreply.github.com>
2022-10-03 09:48:28 -0400

    mudanca de cor ao passar sobre coletaveis

---
+++ Assets/Scripts/Player/PlayerCollectSensor.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollectSensor : MonoBehaviour
+{
+
+    public Material activeMat;
+    public Color originColor;
+
+    private void Start()
+    {
+        originColor = Color.white;
+    }
+
+    // Start is called before the first frame update
+    private void OnTriggerStay(Collider other)
+    {
+        Item i;
+        i = other.transform.GetComponent<Item>();
+        if (i != null)
+        {
+            i.GetComponent<MeshRenderer>().material.color = activeMat.color;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Item i;
+        i = other.transform.GetComponent<Item>();
+        if (i != null)
+        {
+            i.GetComponent<MeshRenderer>().material.color = originColor;
+        }
+    }
+}
