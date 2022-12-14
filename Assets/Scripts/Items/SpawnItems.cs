ec691710d7588ca7dcb24f21159e46edb5cf1dd5
yvy1989 <99916213+yvy1989@users.noreply.github.com>
2022-12-14 20:01:58 -0300

    banana e quando se derruba uma bananeira

--- Assets/Scripts/Items/SpawnItems.cs
+++ Assets/Scripts/Items/SpawnItems.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class SpawnItems : MonoBehaviour
 {
-
+    public Transform spawnLoc;
     public Item spawnedItem;
+    public Item[] spawnedItems;
 
 
     // Start is called before the first frame update
@@ -22,12 +23,20 @@ public class SpawnItems : MonoBehaviour
 
     public void SpanwItem()
     {
+        /*
         for (int i = 0; i < 3; i++)
         {
             Item _spawnedItem = Instantiate(spawnedItem, new Vector3(transform.position.x + Random.Range(0,4), 
                                                                      transform.position.y+0.2f, 
                                                                      transform.position.z + Random.Range(0, 4)), Quaternion.identity);
 
+        }*/
+
+        foreach (var item in spawnedItems)
+        {
+            Item _spawnedItem = Instantiate(item, new Vector3(spawnLoc.position.x + Random.Range(0.5f, 1.4f),
+                                                                     spawnLoc.position.y,
+                                                                     spawnLoc.position.z + Random.Range(0.4f, 2.8f)), Quaternion.identity);
         }
     }
 }
